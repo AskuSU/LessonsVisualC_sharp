2ad9c12a9e55e5ca2079895f5438b014d2f1b648
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-18 22:25:03 +0300

    ListBox
    Файловый Менеджер. Работа с деректориями, путями, файлами

---
+++ ListBox/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Загрузка файлов и папок - кнопка "Перейти"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            foreach (DirectoryInfo crrDir in dirs)
+            {
+                listBox1.Items.Add(crrDir);
+            }
+
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo crrFile in files)
+            {
+                listBox1.Items.Add(crrFile);
+            }
+        }
+
+        //Двойной щелчек по элементу ListBox
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (Path.GetExtension(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())) == "")
+            {
+                textBox1.Text = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString());
+
+                listBox1.Items.Clear();
+
+                DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+
+                DirectoryInfo[] dirs = dir.GetDirectories();
+
+                foreach (DirectoryInfo crrDir in dirs)
+                {
+                    listBox1.Items.Add(crrDir);
+                }
+
+                FileInfo[] files = dir.GetFiles();
+
+                foreach (FileInfo crrFile in files)
+                {
+                    listBox1.Items.Add(crrFile);
+                }
+            }
+            else
+            {
+                Process.Start(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString()));
+            }
+
+            
+        }
+        
+        //Загрузка файлов и папок - кнопка "Назад"
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text[textBox1.Text.Length - 1] == '\\')
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                }
+            }
+            else if (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+            {
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+                }
+            }
+
+            listBox1.Items.Clear();
+
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text);
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            foreach (DirectoryInfo crrDir in dirs)
+            {
+                listBox1.Items.Add(crrDir);
+            }
+
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo crrFile in files)
+            {
+                listBox1.Items.Add(crrFile);
+            }
+        }
+    }
+}
