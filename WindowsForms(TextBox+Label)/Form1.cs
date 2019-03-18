e97f6249e3571623a69ae6f608e2dcc8cefc67a1
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-18 23:54:48 +0300

    WindowsForms(TextBox+Label)
    Работа с TextBox+Label

---
+++ WindowsForms(TextBox+Label)/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace WindowsForms_TextBox_Label_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = "Текст: ";
+
+                if (textBox1.Text != "")
+                {
+                    label1.Text += textBox1.Text;
+                }
+                
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = "Текст: ";
+            }
+            catch (Exception)
+            {
+
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = "Текст: ";
+
+                if (textBox1.Text != "")
+                {
+                    label1.Text += textBox1.Text;
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = "";
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ WindowsForms(TextBox+Label)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_TextBox_Label_
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
