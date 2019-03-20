cc21e7e3108fd7b7a5d9e29e05d7fe5624079e0f
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-20 17:39:26 +0300

    YandexTranslater
    1.0.0

---
+++ YandexTranslate/Form1.Designer.cs
@@ -0,0 +1,423 @@
+﻿namespace YandexTranslate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1025, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 88);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(470, 185);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(543, 88);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(470, 185);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(488, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 49);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(894, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Перевести";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(12, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 39);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Русский";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(137, 43);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 39);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Английский";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(543, 43);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 39);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Немецкий";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(668, 43);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(119, 39);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Французкий";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Азербайджанский",
+            "Азербайджанский",
+            "Албанский",
+            "Амхарский",
+            "Английский",
+            "Арабский",
+            "Армянский",
+            "Африкаанс",
+            "Баскский",
+            "Башкирский",
+            "Белорусский",
+            "Бенгальский",
+            "Бирманский",
+            "Болгарский",
+            "Боснийский",
+            "Валлийский",
+            "Венгерский",
+            "Вьетнамский",
+            "Гаитянский",
+            "Галисийский",
+            "Горномарийский",
+            "Греческий",
+            "Грузинский",
+            "Гуджарати",
+            "Датский",
+            "Иврит",
+            "Идиш",
+            "Индонезийский",
+            "Ирландский",
+            "Исландский",
+            "Испанский",
+            "Итальянский",
+            "Казахский",
+            "Каннада",
+            "Каталанский",
+            "Киргизский",
+            "Китайский",
+            "Корейский",
+            "Коса β",
+            "Кхмерский",
+            "Лаосский β",
+            "Латынь",
+            "Латышский",
+            "Литовский",
+            "Люксембургский",
+            "Македонский",
+            "Малагасийский",
+            "Малайский",
+            "Малаялам",
+            "Мальтийский",
+            "Маори",
+            "Маратхи",
+            "Марийский",
+            "Монгольский",
+            "Немецкий",
+            "Непальский",
+            "Нидерландский",
+            "Норвежский",
+            "Панджаби",
+            "Папьяменто",
+            "Персидский",
+            "Польский",
+            "Португальский",
+            "Румынский",
+            "Русский",
+            "Себуанский",
+            "Сербский",
+            "Сингальский",
+            "Словацкий",
+            "Словенский",
+            "Суахили",
+            "Сунданский",
+            "Тагальский",
+            "Таджикский",
+            "Тайский",
+            "Тамильский",
+            "Татарский",
+            "Телугу",
+            "Турецкий",
+            "Удмуртский",
+            "Узбекский",
+            "Украинский",
+            "Урду",
+            "Финский",
+            "Французский",
+            "Хинди",
+            "Хорватский",
+            "Чешский",
+            "Шведский",
+            "Шотландский (гэльский)",
+            "Эльфийский (синдарин)",
+            "Эмодзи",
+            "Эсперанто",
+            "Эстонский",
+            "Яванский",
+            "Японский"});
+            this.comboBox1.Location = new System.Drawing.Point(262, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(220, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Азербайджанский",
+            "Азербайджанский",
+            "Албанский",
+            "Амхарский",
+            "Английский",
+            "Арабский",
+            "Армянский",
+            "Африкаанс",
+            "Баскский",
+            "Башкирский",
+            "Белорусский",
+            "Бенгальский",
+            "Бирманский",
+            "Болгарский",
+            "Боснийский",
+            "Валлийский",
+            "Венгерский",
+            "Вьетнамский",
+            "Гаитянский",
+            "Галисийский",
+            "Горномарийский",
+            "Греческий",
+            "Грузинский",
+            "Гуджарати",
+            "Датский",
+            "Иврит",
+            "Идиш",
+            "Индонезийский",
+            "Ирландский",
+            "Исландский",
+            "Испанский",
+            "Итальянский",
+            "Казахский",
+            "Каннада",
+            "Каталанский",
+            "Киргизский",
+            "Китайский",
+            "Корейский",
+            "Коса β",
+            "Кхмерский",
+            "Лаосский β",
+            "Латынь",
+            "Латышский",
+            "Литовский",
+            "Люксембургский",
+            "Македонский",
+            "Малагасийский",
+            "Малайский",
+            "Малаялам",
+            "Мальтийский",
+            "Маори",
+            "Маратхи",
+            "Марийский",
+            "Монгольский",
+            "Немецкий",
+            "Непальский",
+            "Нидерландский",
+            "Норвежский",
+            "Панджаби",
+            "Папьяменто",
+            "Персидский",
+            "Польский",
+            "Португальский",
+            "Румынский",
+            "Русский",
+            "Себуанский",
+            "Сербский",
+            "Сингальский",
+            "Словацкий",
+            "Словенский",
+            "Суахили",
+            "Сунданский",
+            "Тагальский",
+            "Таджикский",
+            "Тайский",
+            "Тамильский",
+            "Татарский",
+            "Телугу",
+            "Турецкий",
+            "Удмуртский",
+            "Узбекский",
+            "Украинский",
+            "Урду",
+            "Финский",
+            "Французский",
+            "Хинди",
+            "Хорватский",
+            "Чешский",
+            "Шведский",
+            "Шотландский (гэльский)",
+            "Эльфийский (синдарин)",
+            "Эмодзи",
+            "Эсперанто",
+            "Эстонский",
+            "Яванский",
+            "Японский"});
+            this.comboBox2.Location = new System.Drawing.Point(793, 48);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(220, 24);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 459);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Yandex Переводчик";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
