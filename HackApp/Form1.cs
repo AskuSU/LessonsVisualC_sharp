e9e339a33d7a56b27ee4f22eeaa6f2ec4411a408
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-21 14:01:01 +0300

    HackApp
    Для тестов в net Reflector

---
+++ HackApp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HackApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "12345")
+            {
+                textBox1.Clear();
+
+                this.Hide();
+
+                Main main = new Main();
+
+                main.Show();
+            }
+            else
+            {
+                textBox1.Clear();
+
+                MessageBox.Show("Введен не верный пароль", "Ошибка входа!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+            }
+            
+        }
+    }
+}
