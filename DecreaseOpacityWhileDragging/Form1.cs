dd610b544647967ec66bce94291c33c0e3d558d2
Andrey Zakharenko <Zaharandrei15@mail.ru>
2019-03-26 21:49:57 +0300

    DecreaseOpacityWhileDragging
    Установка прозрачности окна при ее перетаскивании и зумировании

---
+++ DecreaseOpacityWhileDragging/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecreaseOpacityWhileDragging
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            Opacity = 0.5;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            Opacity = 1;
+        }
+    }
+}
